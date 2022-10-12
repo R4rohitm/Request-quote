695ece8209d352ba5fe214604179f3618fe8a658
Saradhi <saradhi0001069@gmail.com>
2022-10-12 12:57:14 +0530

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RequestQuote from './components/RequestQuote';
+
+function App() {
+  return (
+   <>
+   <RequestQuote/>
+   </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/RequestQuote.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./styles.css";
+const RequestQuote = () => {
+  return (
+    <>
+    <div className='header'>
+    </div>
+    <div className='Quote'>
+       <div>
+          <h1>Request a quote</h1>
+          <h3>Cargo details</h3>
+          <h3>Product:</h3>
+          <input type="text" />
+       </div>
+    </div>
+    </>
+  )
+}
+
+export default RequestQuote
