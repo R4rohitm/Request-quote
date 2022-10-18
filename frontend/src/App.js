c0a633ab3b37af3ef97eb803c03a6233ff7f2322
Saradhi <saradhi0001069@gmail.com>
2022-10-18 14:10:28 +0530

    Admin pannel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import RequestQuote2 from "./components/RequestQuote2";
+import {Routes,Route} from "react-router-dom";
+import Admin from "./components/Admin";
+
 
 function App() {
   return (
     <>
-      <RequestQuote2 />
+      {/* <RequestQuote2 /> */}
+      <Routes>
+        <Route path="/requestquote" element={<RequestQuote2/>}/>
+        <Route path="/admin" element={<Admin/>}/>
+      </Routes>
     </>
   );
 }
